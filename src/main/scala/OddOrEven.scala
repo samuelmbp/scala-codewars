e28f832cafefe99b7c90a9e05f013967ba251535
Samuel Raducan <samuel.thembp@gmail.com>
2025-07-14 10:28:33 +0200

    Add odd or even coding problem

---
+++ src/main/scala/OddOrEven.scala
@@ -0,0 +1,34 @@
+/*
+Given a list of integers, determine whether the sum of its
+elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as:
+[0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+ */
+
+object OddOrEven extends App {
+
+  def oddOrEven(xs: Seq[Int]): String = {
+    if (xs.isEmpty) Seq(0)
+
+    val sum = xs.sum
+    if (sum % 2 == 0) "even"
+    else "odd"
+  }
+
+  println(oddOrEven(Seq(0)))
+  println(oddOrEven(Seq(0, 1, 4)))
+  println(oddOrEven(Seq(0, -1, -5)))
+}
